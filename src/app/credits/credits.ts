d2e5ce5f889621fc6fce702c3ce60831972939ca
Zach Boone <zach.boone@fulfil.ai>
2023-12-28 22:28:06 -0500

    credits and misc fixes

--- src/app/credits/credits.ts
+++ src/app/credits/credits.ts
@@ -9,24 +9,58 @@ export class Credits extends Phaser.Scene {
   create() {
     const centerX = this.cameras.main.centerX;
     const centerY = this.cameras.main.centerY;
-    const creditsText = `Thank you for playing\nHappy Adventures\nby James Boone`;
-
+    const creditsStartY = centerY + 400;
     const credits = this.add
-      .text(centerX, centerY + 100, creditsText, {
+      .text(centerX, creditsStartY, this.creditsText(), {
         font: "36px Arial",
         align: "center",
       })
       .setOrigin(0.5, 0.5);
 
-    // Scroll the credits up
+    const distanceY = creditsStartY + credits.height;
+    const speed = 0.1;
+    const duration = distanceY / speed;
+
     this.tweens.add({
       targets: credits,
-      y: 0, // Adjust depending on the length of the credits
+      y: (-1 * credits.height) / 2, // Adjust depending on the length of the credits
       ease: "Linear",
-      duration: 12_000,
+      duration,
       onComplete: () => {
         this.scene.start(Scene.TheEnd);
       },
     });
+  }
+
+  private creditsText(): string {
+    const lineBreak = Symbol("lineBreak");
+    const textItems: (string | typeof lineBreak)[] = [
+      "Thank you for playing",
+      "Happy Adventures",
+      lineBreak,
+      lineBreak,
+      lineBreak,
+      "Designers",
+      "James Boone",
+      "Boone Stern",
+      lineBreak,
+      "Testers",
+      "Levi Boone",
+      lineBreak,
+      "Developers",
+      "Zach Boone",
+    ];
+    let text = "";
+    textItems.forEach((textItem, i) => {
+      if (textItem === lineBreak) {
+        text += "\n";
+      } else {
+        text += `${textItem}`;
+        if (i !== textItems.length - 1) {
+          text += "\n";
+        }
+      }
+    });
+    return text;
   }
 }
